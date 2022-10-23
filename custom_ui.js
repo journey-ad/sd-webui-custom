428b8d1ce038d3ffdc66079049db31d8366bb2a5
journey-ad <journey.adc@gmail.com>
2022-10-24 00:32:08 +0800

    添加prompt标签翻译

--- custom_ui.js
+++ custom_ui.js
@@ -73,6 +73,74 @@
     }
   }
 
+  let tagsI18n = null
+  // 翻译标签
+  function translatePrompt(text) {
+    if (!tagsI18n) {
+      let _tmp_list = loadCSV('ui_custom/tags_i18n.csv')
+      tagsI18n = _tmp_list.reduce((tagsI18n, [tag, _, i18n]) => {
+        if (!tag || !i18n || tag === i18n) return tagsI18n // 过滤无效数据
+
+        tagsI18n[tag] = i18n
+        return tagsI18n
+      }, {})
+    }
+
+    // 转小写并去除多余空格和权重字符，保留转义括号
+    const promptText = text.toLowerCase()
+      .replace(/\s\s+/g, ' ')
+      .replace(/(?<!\\)[(){}\[\]]/g, '')
+
+    // 按逗号分割匹配翻译
+    const promptList = promptText
+      .split(/,|，/)
+      .map(tag => {
+        tag = tag.trim()
+        let parsed_tag = tag.replace(/\s+/g, '_')
+        return tagsI18n[parsed_tag] || tag
+      })
+
+    const promptTrans = promptList.join(', ')
+
+    return promptTrans
+  }
+
+  // 节流函数
+  function throttle(fn, delay) {
+    let timer = null
+
+    return function () {
+      if (timer) return
+
+      fn.apply(this, arguments)
+
+      timer = setTimeout(() => {
+        timer = null
+      }, delay)
+    }
+  }
+
+  // 防抖函数
+  function debounce(fn, delay) {
+    let ts = 0, timer = null
+
+    return function () {
+      if (Date.now() - ts < delay) {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+          fn.apply(this, arguments)
+        }, delay)
+
+        return
+      }
+
+      clearTimeout(timer)
+      fn.apply(this, arguments)
+
+      ts = Date.now()
+    }
+  }
+
   function querySelector(...args) {
     return gradioApp()?.querySelector(...args)
   }
@@ -98,6 +166,8 @@
     const event = new Event('input')
     Object.defineProperty(event, 'target', { writable: false, value: el });
     el.dispatchEvent(event)
+
+    el.dispatchEvent(new Event('focusout')) // 兼容tagAutocomplete插件
   }
 
   // 通过标签修改值
@@ -115,6 +185,49 @@
     request.open("GET", `file/${filePath}`, false);
     request.send(null);
     return request.responseText;
+  }
+
+  // Load CSV
+  function loadCSV(path) {
+    let text = readFile(path);
+    return parseCSV(text);
+  }
+
+  // Parse the CSV file into a 2D array. Doesn't use regex, so it is very lightweight.
+  function parseCSV(str) {
+    var arr = [];
+    var quote = false;  // 'true' means we're inside a quoted field
+
+    // Iterate over each character, keep track of current row and column (of the returned array)
+    for (var row = 0, col = 0, c = 0; c < str.length; c++) {
+      var cc = str[c], nc = str[c + 1];        // Current character, next character
+      arr[row] = arr[row] || [];             // Create a new row if necessary
+      arr[row][col] = arr[row][col] || '';   // Create a new column (start with empty string) if necessary
+
+      // If the current character is a quotation mark, and we're inside a
+      // quoted field, and the next character is also a quotation mark,
+      // add a quotation mark to the current column and skip the next character
+      if (cc == '"' && quote && nc == '"') { arr[row][col] += cc; ++c; continue; }
+
+      // If it's just one quotation mark, begin/end quoted field
+      if (cc == '"') { quote = !quote; continue; }
+
+      // If it's a comma and we're not in a quoted field, move on to the next column
+      if (cc == ',' && !quote) { ++col; continue; }
+
+      // If it's a newline (CRLF) and we're not in a quoted field, skip the next character
+      // and move on to the next row and move to column 0 of that new row
+      if (cc == '\r' && nc == '\n' && !quote) { ++row; col = 0; ++c; continue; }
+
+      // If it's a newline (LF or CR) and we're not in a quoted field,
+      // move on to the next row and move to column 0 of that new row
+      if (cc == '\n' && !quote) { ++row; col = 0; continue; }
+      if (cc == '\r' && !quote) { ++row; col = 0; continue; }
+
+      // Otherwise, append the current character to the current column
+      arr[row][col] += cc;
+    }
+    return arr;
   }
 
   // 模拟点击重载gradio
@@ -151,6 +264,23 @@
 
     // UI更新时翻译页面
     onUiUpdate(translatePage)
+
+    // 添加prompt翻译框
+    querySelectorAll('#txt2img_prompt textarea, #img2img_prompt textarea').forEach(el => {
+      const $wrapper = document.createElement('div')
+      $wrapper.className = 'custom_ui__prompt_trans text-sm text-gray-400'
+
+      // 内容变动时执行翻译
+      el.addEventListener('input', debounce((e) => {
+        const text = el.value.trim()
+
+        const promptTrans = translatePrompt(text)
+
+        $wrapper.textContent = promptTrans
+      }, 500))
+
+      el.insertAdjacentElement('afterend', $wrapper)
+    })
 
     // 添加交换宽高按钮
     const $sizeSettingPanel = querySelectorByLabel('Width')?.closest('.gr-form')
